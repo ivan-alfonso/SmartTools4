13699f7ea7429d6980935544ea8abf8cb50c6d99
ivan-alfonso <ivandaalf@hotmail.com>
2016-11-01 18:59:07 -0500

    MemCachier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,5 +97,17 @@ Rails.application.configure do
   :enable_starttls_auto => true
   }
 
+
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }
+
+
   config.logger = RemoteSyslogLogger.new('logs4.papertrailapp.com', 52836)
 end
