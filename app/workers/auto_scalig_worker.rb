889a5cc39a93f8d2163a9a11e0a30fc2311cafff
ivan-alfonso <ivandaalf@hotmail.com>
2016-11-04 19:11:25 -0500

    worker_autoscaling

--- app/workers/auto_scalig_worker.rb
+++ app/workers/auto_scalig_worker.rb
@@ -11,24 +11,16 @@ class AutoScaligWorker
     numeroMensajesSQS = count_messages_from_queue().to_i
 
     if ( numeroMensajesSQS == 0 )
-       heroku.post_ps_scale('smarttools4', 'worker', 1)
+       heroku.post_ps_scale('smarttools4', 'worker', 0)
     elsif ( numeroMensajesSQS < 3 )
 	     heroku.post_ps_scale('smarttools4', 'worker', 1)
-    end
-
-=begin
     elsif ( numeroMensajesSQS < 5 )
-        heroku.post_ps_scale('smarttools4', 'worker', 2)
-    end
-
+       heroku.post_ps_scale('smarttools4', 'worker', 2)
     elsif ( numeroMensajesSQS < 7 )
-        heroku.post_ps_scale('smarttools4', 'worker', 3)
+       heroku.post_ps_scale('smarttools4', 'worker', 3)
+    elsif ( numeroMensajesSQS >6 )
+       heroku.post_ps_scale('smarttools4', 'worker', 4)
     end
-
-    elsif ( numeroMensajesSQS > 6 )
-        heroku.post_ps_scale('smarttools4', 'worker', 4)
-    end
-=end
 
   end
 
