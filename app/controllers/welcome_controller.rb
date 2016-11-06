6b265c0b1c0117fc3d0544bb09b4505d0c3ff884
ivan-alfonso <ivandaalf@hotmail.com>
2016-11-06 17:36:19 -0500

    prueba memcachier

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,8 @@ class WelcomeController < ApplicationController
   def index
   	@host = Resolv.getname(request.remote_ip)
   	@hostsocket = Socket.gethostname
+
+  	ip=Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+	@iptest = ip.ip_address
   end
 end
