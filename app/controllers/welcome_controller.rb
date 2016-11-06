0da9c9727f422c0ee2e6e35b9ce80c65bffea49c
ivan-alfonso <ivandaalf@hotmail.com>
2016-11-06 17:24:30 -0500

    prueba memcachier

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 require 'resolv'
+require 'socket'
 
 class WelcomeController < ApplicationController 
   def index
   	@host = Resolv.getname(request.remote_ip)
+  	@hostsocket = Socket.gethostname
   end
 end
